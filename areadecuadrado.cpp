3cba5acdcfa4de67d196471df8f4ba7440081fd0
Ricardo Villanueva <143663616+Ricardovillan@users.noreply.github.com>
2023-09-12 17:08:53 -0600

    Add files via upload

--- areadecuadrado.cpp
+++ areadecuadrado.cpp
@@ -1,17 +1,18 @@
 // areadecuadrado.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
 //
-
+#include <cmath>
 #include <stdio.h>
+#include <iostream>
 
 void main()
 {
-    int Altura, Base, area;
-    printf("Base:");
-    scanf_s("%i",&Base);
-    printf("Altura:");
-    scanf_s("%i", &Altura);
-    area = Base * Altura;
-    printf("El area del rectangulo es:%i", area);
+    float radio, area;
+    float π = 3.1416;
+    printf("Radio:");
+    scanf_s("%f",&radio);
+    float radio2 = radio * radio;
+    area = (π * radio2);
+    printf("El area del rectangulo es:%f", area);
 }
 
 
